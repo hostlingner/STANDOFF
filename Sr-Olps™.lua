22e481294ca2168b9fa94c3ceb7ee6a426ba6e53
hostlingner <117482363+hostlingner@users.noreply.github.com>
2023-06-16 10:32:11 -0300

    Update Sr-Olps™.lua

--- Sr-Olps™.lua
+++ Sr-Olps™.lua
@@ -12,7 +12,7 @@ os.exit()
 end
 
 local v = gg.getTargetInfo()
-if v.versionCode ~= 3051 then
+if v.versionCode ~= 3061 then
 print('Este script só funciona com a versão do jogo 0.23.2 Você tem a versão do jogo', v.versionCode, 'Instale a versão 0.23.2 e tente novamente.')
 os.exit()
 end
